7e186e8ee11c872cebdc48f7e0f78253a258e278
Min Hu <lebonthe@gmail.com>
2023-09-19 16:37:22 +0800

    Initial Commit

---
+++ Guess Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess Number
+//
+//  Created by Min Hu on 2023/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
